938cc909f76af407e9e2a6a8b2596fb69a55b897
richo225 <richo225@gmail.com>
2017-03-14 23:09:40 +0000

    Seed all_cards JSON data to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,36 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }"])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+all_cards = JSON.parse(File.read("#{Rails.root}/db/data/all_cards.json"))
+puts "Seeding all cards........"
+all_cards.each do |a|
+  c = Card.new
+  c.artist = a["artist"]
+  c.attack = a["attack"]
+  c.card_class = a["cardClass"]
+  c.classes = a["classes"]
+  c.collectible = a["collectible"]
+  c.cost = a["cost"]
+  c.dbf_id = a["dbfId"]
+  c.durability = a["durability"]
+  c.elite = a["elite"]
+  c.entourage = a["entourage"]
+  c.flavour = a["flavor"]
+  c.health = a["health"]
+  c.how_to_earn = a["howToEarn"]
+  c.how_to_earn_golden = a["howToEarnGolden"]
+  c.card_id = a["cardId"]
+  c.mechanics = a["mechanics"]
+  c.multiClassGroup = a["multiClassGroup"]
+  c.name = a["name"]
+  c.play_requirements = a["playRequirements"]
+  c.player_class = a["playerClass"]
+  c.rarity = a["rarity"]
+  c.set = a["set"]
+  c.text = a["text"]
+  c.type = a["type"]
+  c.save!
+end
